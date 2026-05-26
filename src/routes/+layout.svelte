3674570a50cc71cf614a4afddb3adea17207074c
Hernán Oliva <hhernanoliva@gmail.com>
2026-05-26 17:45:21 -0300

    feat: add Lenis smooth scroll to layout shell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Lenis from 'lenis';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
+
+	$effect(() => {
+		const lenis = new Lenis();
+
+		function raf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+		const id = requestAnimationFrame(raf);
+
+		return () => {
+			lenis.destroy();
+			cancelAnimationFrame(id);
+		};
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<Navbar />
+<main>
+	{@render children()}
+</main>
+<Footer />
